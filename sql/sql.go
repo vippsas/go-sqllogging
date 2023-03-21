fe49f8c11b3381695b899dd7335bf1d91ecfe5c0
Vaidas Macijauskas <vaidas.macijauskas@vippsmobilepay.com>
2023-03-21 08:34:44 +0200

    fix: add description

--- sql/sql.go
+++ sql/sql.go
@@ -1,3 +1,6 @@
+// Package sql allows pulling in the SQL files present in this directory as a go library.
+// The SQL files follows the conventions of the sqlcode tool:
+// https://github.com/vippsas/sqlcode
 package sql
 
 import (
